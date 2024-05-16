803fa37c319e9fd7448fadbf13e72e02b00c8ae3
m0in92 <moinahmed100@gmail.com>
2024-05-16 11:29:45 -0400

    initiated Kalman Filter in CPP

--- bmslogic/calc_helpers/kalman_filter.cpp
+++ bmslogic/calc_helpers/kalman_filter.cpp
@@ -5,3 +5,11 @@ NormalRandomVector::NormalRandomVector(std::vector<std::vector<double>> i_vec_in
     : m_vec(i_vec_init), m_cov(i_cov_init)
 {
 }
+
+SigmaPointKalmanFilter::SigmaPointKalmanFilter(NormalRandomVector i_X, NormalRandomVector i_W, NormalRandomVector i_V, int y_dim,
+                                               std::function<NormalRandomVector(NormalRandomVector, NormalRandomVector, NormalRandomVector)> state_equation,
+                                               std::function<NormalRandomVector(NormalRandomVector, NormalRandomVector, NormalRandomVector)> output_equation,
+                                               std::string method_type) : m_X(i_X), m_W(i_W), m_V(i_V), m_y_dim(y_dim),
+                                                                          m_state_equation(state_equation), m_output_equation(output_equation), m_method_type(method_type)
+{
+}
--- bmslogic/calc_helpers/kalman_filter.h
+++ bmslogic/calc_helpers/kalman_filter.h
@@ -1,3 +1,5 @@
+#include <functional>
+#include <string>
 #include <vector>
 
 class NormalRandomVector
@@ -15,4 +17,25 @@ public:
 private:
     std::vector<std::vector<double>> m_vec;
     std::vector<std::vector<double>> m_cov;
+};
+
+class SigmaPointKalmanFilter
+{
+public:
+    SigmaPointKalmanFilter(NormalRandomVector i_X, NormalRandomVector i_W, NormalRandomVector i_V, int y_dim,
+                           std::function<NormalRandomVector(NormalRandomVector, NormalRandomVector, NormalRandomVector)> state_equation,
+                           std::function<NormalRandomVector(NormalRandomVector, NormalRandomVector, NormalRandomVector)> output_equation,
+                           std::string method_type);
+
+private:
+    NormalRandomVector m_X;
+    NormalRandomVector m_W;
+    NormalRandomVector m_V;
+
+    int m_y_dim;
+
+    std::function<NormalRandomVector(NormalRandomVector, NormalRandomVector, NormalRandomVector)> m_state_equation;
+    std::function<NormalRandomVector(NormalRandomVector, NormalRandomVector, NormalRandomVector)> m_output_equation;
+
+    std::string m_method_type;
 };
