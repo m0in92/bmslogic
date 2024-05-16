3fa470ef28aced171a7d85307c23e1e966ddfaae
m0in92 <moinahmed100@gmail.com>
2024-05-16 18:50:54 -0400

    worked more on Kalman Filter

--- bmslogic/calc_helpers/binding_cpp_py.cpp
+++ bmslogic/calc_helpers/binding_cpp_py.cpp
@@ -1,6 +1,8 @@
+#include <functional>
 #include <vector>
 
 #include "pybind11/pybind11.h"
+#include "pybind11/functional.h"
 #include "pybind11/stl.h"
 
 #include "kalman_filter.h"
@@ -11,9 +13,22 @@ PYBIND11_MODULE(calc_helpers, m)
 {
     m.doc() = "Contains the functions and classes that aid in the simulation calculations.";
 
+    // Normal Random Vector
     py::class_<NormalRandomVector>(m, "NormalRandomVector")
         .def(py::init<std::vector<std::vector<double>>, std::vector<std::vector<double>>>(), py::arg("vector_init"), py::arg("cov_init"))
-        .def_property("get_mean", &NormalRandomVector::get_vec, &NormalRandomVector::set_vec)
+        .def_property("get_vec", &NormalRandomVector::get_vec, &NormalRandomVector::set_vec)
         .def_property("get_cov", &NormalRandomVector::get_cov, &NormalRandomVector::set_cov)
         .def_property_readonly("get_dim", &NormalRandomVector::get_dim);
+
+    // Sigma Point Kalman Filter
+    py::class_<SigmaPointKalmanFilter>(m, "SigmaPointKalmanFilter")
+        .def(py::init<NormalRandomVector, NormalRandomVector, NormalRandomVector, int,
+                      std::function<NormalRandomVector(NormalRandomVector, NormalRandomVector, NormalRandomVector)>,
+                      std::function<NormalRandomVector(NormalRandomVector, NormalRandomVector, NormalRandomVector)>,
+                      std::string>(),
+                      py::arg("X"), py::arg("W"), py::arg("V"), py::arg("y_dim"), 
+                      py::arg("state_equation"), py::arg("output_equation"), py::arg("method_type")="CDKF")
+        .def_property_readonly("X", &SigmaPointKalmanFilter::get_X)
+        .def_property_readonly("W", &SigmaPointKalmanFilter::get_W)
+        .def_property_readonly("V", &SigmaPointKalmanFilter::get_V);
 }
--- tests/test_calc_helpers.py
+++ tests/test_calc_helpers.py
@@ -2,7 +2,14 @@ import unittest
 
 import numpy as np
 
-from bmslogic.calc_helpers.calc_helpers import NormalRandomVector
+from bmslogic.calc_helpers.calc_helpers import NormalRandomVector, SigmaPointKalmanFilter
+
+
+def func_f(x_k, u_k, w_k):
+    return np.sqrt(5 + x_k) + w_k
+
+def func_h(x_k, u_k, v_k):
+    return x_k ** 3 + v_k
 
 
 class TestPyNormalRandomVector(unittest.TestCase):
@@ -11,7 +18,53 @@ class TestPyNormalRandomVector(unittest.TestCase):
 
     def test_constructor(self):
         rv = NormalRandomVector(vector_init=self.mean, cov_init=self.cov)
-        self.assertTrue(np.array_equal(self.mean, rv.get_mean))
+        self.assertTrue(np.array_equal(self.mean, rv.get_vec))
         self.assertTrue(np.array_equal(self.cov, rv.get_cov))
         self.assertEqual(len(self.mean), rv.get_dim)
 
+
+class TestSPKFProperties(unittest.TestCase):
+    vector_x = np.array([2, 2]).reshape(-1, 1)
+    vector_w = np.array([0]).reshape(-1, 1)
+    vector_v = np.array([0]).reshape(-1, 1)
+
+    vector_x2 = np.array([2]).reshape(-1, 1)
+    vector_w2 = np.array([0]).reshape(-1, 1)
+    vector_v2 = np.array([0]).reshape(-1, 1)
+
+    cov_x = np.array([[2, 0], [0, 2]])
+    cov_w = np.array([[1]])
+    cov_v = np.array([[2]])
+
+    cov_x2 = np.array([[1]])
+    cov_w2 = np.array([[1]])
+    cov_v2 = np.array([[2]])
+
+    x = NormalRandomVector(vector_init=vector_x, cov_init=cov_x)
+    w = NormalRandomVector(vector_init=vector_w, cov_init=cov_w)
+    v = NormalRandomVector(vector_init=vector_v, cov_init=cov_v)
+
+    x2 = NormalRandomVector(vector_init=vector_x2, cov_init=cov_x2)
+    w2 = NormalRandomVector(vector_init=vector_w2, cov_init=cov_w2)
+    v2 = NormalRandomVector(vector_init=vector_v2, cov_init=cov_v2)
+
+    y_dim = 1
+
+    # @staticmethod
+    # def func_f(x_k, u_k, w_k):
+    #     return np.sqrt(5 + x_k) + w_k
+
+    # @staticmethod
+    # def func_h(x_k, u_k, v_k):
+    #     return x_k ** 3 + v_k
+
+    spkf_instance: SigmaPointKalmanFilter = SigmaPointKalmanFilter(X=x, W=w, V=v, y_dim=y_dim,
+                                                                   state_equation=func_f, output_equation=func_h)
+    spkf_instance2: SigmaPointKalmanFilter = SigmaPointKalmanFilter(X=x2, W=w2, V=v2, y_dim=y_dim,
+                                                                    state_equation=func_f, output_equation=func_h)
+    
+    def test_constructor(self):
+        self.assertTrue(np.array_equal(self.vector_x, np.array(self.spkf_instance.X.get_vec)))
+        self.assertTrue(np.array_equal(self.vector_w, self.spkf_instance.W.get_vec))
+        self.assertTrue(np.array_equal(self.vector_v, self.spkf_instance.V.get_vec))
+        # self.assertEqual(self.y_dim, self.spkf_instance.y_dim)
