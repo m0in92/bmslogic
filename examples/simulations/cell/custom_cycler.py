0273501747dcd421ab7d9cdecd20f86186b4484b
m0in92 <moinahmed100@gmail.com>
2024-06-18 22:33:00 -0400

    updated.

--- examples/simulations/cell/custom_cycler.py
+++ examples/simulations/cell/custom_cycler.py
@@ -6,14 +6,33 @@ __author__ = "Moin Ahmed"
 __copyright__ = 'Copyright 2024 by SPCPPy. All rights reserved.'
 __status__ = 'deployed'
 
-import time
+import pathlib
+import sys
 
 import numpy as np
 import matplotlib.pyplot as plt
 
-import SPCPPY as sp
+try:
+    import bmslogic.simulations.cell.cell as cell_sim
+    from bmslogic.simulations.cell.parameter_set_manager import ParameterSets
+    from bmslogic.simulations.cell.plot import Plot
 
-from examples.example_parameter import *
+    from examples.simulations.cell.example_parameter import *
+
+except ModuleNotFoundError as e:
+    PROJECT_DIR: str = pathlib.Path(
+        __file__).parent.parent.parent.parent.__str__()
+    print(PROJECT_DIR)
+    sys.path.append(PROJECT_DIR)
+
+    from bmslogic import cell_sim
+    import bmslogic.simulations.cell.cell as cell_sim
+    from bmslogic.simulations.cell.parameter_set_manager import ParameterSets
+    from bmslogic.simulations.cell.plot import Plot
+
+    from examples.simulations.cell.example_parameter import *
+
+from example_parameter import *
 
 
 if __name__ == "__main__":
@@ -25,24 +44,21 @@ if __name__ == "__main__":
     V_max: float = 4.2
     soc_lib: float = 0.0
 
-    battery_cell: sp.BatteryCell = sp.ParameterSets(name='test').generate_BatteryCell_instance(soc_n_init=SOC_init_n,
-                                                                                               soc_p_init=SOC_init_p,
-                                                                                               T_amb=T,
-                                                                                               R_cell=R_cell)
-    
+    battery_cell: cell_sim.BatteryCell = ParameterSets(name='test').generate_BatteryCell_instance(soc_n_init=SOC_init_n,
+                                                                                                  soc_p_init=SOC_init_p,
+                                                                                                  T_amb=T,
+                                                                                                  R_cell=R_cell)
+
     t: np.ndarray = np.linspace(0, 5000, 100 * 3601)
     I: np.ndarray = -1.656 * np.ones(len(t))
-    cycler: sp.CustomCycler = sp.CustomCycler(t_array=t, current_array=I,
-                                              V_min=V_min, V_max=V_max,
-                                              soc_lib_min=soc_lib_min, soc_lib_max=soc_lib_max,
-                                              soc_lib=soc_lib)
-    solver: sp.BatterySolver =sp.BatterySolver(battery_cell=battery_cell, 
-                                                  is_isothermal=True, 
-                                                  enable_degradation=False)
-    sol: sp.Solution = solver.solve(cycler=cycler)
-    
+    cycler: cell_sim.CustomCycler = cell_sim.CustomCycler(t_array=t, current_array=I,
+                                                          V_min=V_min, V_max=V_max,
+                                                          soc_lib_min=soc_lib_min, soc_lib_max=soc_lib_max,
+                                                          soc_lib=soc_lib)
+    solver: cell_sim.BatterySolver = cell_sim.BatterySolver(battery_cell=battery_cell,
+                                                            is_isothermal=True,
+                                                            enable_degradation=False)
+    sol: cell_sim.Solution = solver.solve(cycler=cycler)
+
     # plots
-    sp.Plot(sol=sol).plot_comprehensive()
-
-    
-
+    Plot(sol=sol).plot_comprehensive()
