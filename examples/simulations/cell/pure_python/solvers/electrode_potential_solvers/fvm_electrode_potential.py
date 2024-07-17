dbb9e3015f95a9fd8347838392c64273730f368f
m0in92 <moinahmed100@gmail.com>
2024-07-16 21:35:52 -0400

    update

--- examples/simulations/cell/pure_python/solvers/electrode_potential_solvers/fvm_electrode_potential.py
+++ examples/simulations/cell/pure_python/solvers/electrode_potential_solvers/fvm_electrode_potential.py
@@ -9,9 +9,22 @@ __status__ = "deployed"
 import numpy as np
 import matplotlib.pyplot as plt
 
-from SPPy.models.battery import SPMe
-from SPPy.solvers.co_ordinates import ElectrolyteFVMCoordinates
-from SPPy.solvers.electrode_potential import ElectrodePotentialFVMSolver
+try:
+    from bmslogic.simulations.cell.models import PySPMe
+    from bmslogic.simulations.cell.solvers.coords import PyElectrolyteFVMCoordinates
+    from bmslogic.simulations.cell.solvers.electrode_potential import PyElectrodePotentialFVMSolver
+except ModuleNotFoundError:
+    import sys
+    import os
+    import pathlib
+    PROJECT_DIR: str = pathlib.Path(
+        __file__).parent.parent.parent.parent.parent.parent.parent.__str__()
+    print(PROJECT_DIR)
+    sys.path.append(PROJECT_DIR)
+    from bmslogic.simulations.cell.models import PySPMe
+    from bmslogic.simulations.cell.solvers.coords import PyElectrolyteFVMCoordinates
+    from bmslogic.simulations.cell.solvers.electrode_potential import PyElectrodePotentialFVMSolver
+
 
 L_n: float = 8e-5
 L_s: float = 2.5e-5
@@ -30,20 +43,27 @@ sigma_n: float = 100
 sigma_eff_p: float = sigma_p * (epsilon_ep ** brugg_p)
 sigma_eff_n: float = sigma_n * (epsilon_ep ** brugg_n)
 
-coords: ElectrolyteFVMCoordinates = ElectrolyteFVMCoordinates(L_n=8e-5, L_s=2.5e-5, L_p=8.8e-5)
-fvm_solver_p: ElectrodePotentialFVMSolver = ElectrodePotentialFVMSolver(fvm_coords=coords,
+coords: PyElectrolyteFVMCoordinates = PyElectrolyteFVMCoordinates(L_n=8e-5, L_s=2.5e-5, L_p=8.8e-5)
+fvm_solver_p: PyElectrodePotentialFVMSolver = PyElectrodePotentialFVMSolver(fvm_coords=coords,
                                                                         electrode_type='p',
                                                                         sigma_eff=sigma_eff_p, a_s=a_s_p)
-fvm_solver_n: ElectrodePotentialFVMSolver = ElectrodePotentialFVMSolver(fvm_coords=coords,
+fvm_solver_n: PyElectrodePotentialFVMSolver = PyElectrodePotentialFVMSolver(fvm_coords=coords,
                                                                         electrode_type='n',
                                                                         sigma_eff=sigma_eff_n, a_s=a_s_n)
-j_n: np.ndarray = SPMe.molar_flux_electrode(I=-1.656, S=0.7824, electrode_type='n') * np.ones(len(coords.array_x_n))
+j_n: np.ndarray = PySPMe.molar_flux_electrode(I=-1.656, S=0.7824, electrode_type='n') * np.ones(len(coords.array_x_n))
 # [mol/m2/s]
-j_p: np.ndarray = SPMe.molar_flux_electrode(I=-1.656, S=0.7824, electrode_type='n') * np.ones(len(coords.array_x_n))
+j_p: np.ndarray = PySPMe.molar_flux_electrode(I=-1.656, S=0.7824, electrode_type='n') * np.ones(len(coords.array_x_n))
 # [mol/m2/s]
 
-plt.plot(coords.array_x_n, fvm_solver_n.solve_phi_s(j=j_n, terminal_potential=0.0).flatten())
-plt.plot(coords.array_x_p, fvm_solver_p.solve_phi_s(j=j_p, terminal_potential=4.2).flatten())
+
+# plots
+fig = plt.figure()
+
+ax1 = fig.add_subplot(121)
+ax1.plot(coords.array_x_n, fvm_solver_n.solve_phi_s(j=j_n, terminal_potential=0.0).flatten())
+
+ax2 = fig.add_subplot(122)
+ax2.plot(coords.array_x_p, fvm_solver_p.solve_phi_s(j=j_p, terminal_potential=4.2).flatten())
 
 plt.ticklabel_format(axis='x', scilimits=[-1, 1])
 plt.show()
