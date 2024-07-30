f48439b6047c80bf17b718aa26bfbdcb4c501aab
m0in92 <moinahmed100@gmail.com>
2024-07-30 13:50:39 -0400

    added tests for CNSolver

--- cpp_tests/test_solvers.cpp
+++ cpp_tests/test_solvers.cpp
@@ -1,5 +1,6 @@
 #include "gtest/gtest.h"
 
+#include "extern/owl.h"
 #include "solvers.h"
 
 TEST(BaseConcSolverTest, Constructor)
@@ -35,10 +36,58 @@ TEST(PolynomialApprox, Solve)
 
     char electrode_type = 'n';
     std::string solver_type = "higher";
-    PolynomialApprox solver_instance = PolynomialApprox(electrode_type, c_max*SOC_init, solver_type);
-    
+    PolynomialApprox solver_instance = PolynomialApprox(electrode_type, c_max * SOC_init, solver_type);
+
     solver_instance.solve(dt, t_prev, I_app, R, S, D);
     EXPECT_EQ(solver_instance.get_c_surf() / c_max, 0.7504676658078573);
     solver_instance.solve(dt, t_prev, I_app, R, S, D);
     EXPECT_EQ(solver_instance.get_c_surf() / c_max, 0.750422969925152);
+}
+
+TEST(CNSolverTest, Constructor)
+{
+    double c_init = 0.45 * 51410;
+    char electrode_type = 'p';
+    double num_spatial_pts = 100;
+
+    CNSolver solver_instance = CNSolver(c_init, electrode_type, num_spatial_pts);
+
+    EXPECT_EQ(solver_instance.get_electrode_type(), electrode_type);
+    EXPECT_EQ(solver_instance.get_spatial_pts(), num_spatial_pts);
+    EXPECT_EQ(solver_instance.get_c_s_surf(), c_init);
+    EXPECT_EQ(solver_instance.get_c_surf(), c_init);
+
+    OWL::ArrayXD c_prev = c_init * OWL::Ones(num_spatial_pts);
+    std::vector<double> c_prev_ = c_prev.getArray();
+    EXPECT_EQ(solver_instance.get_c_prev(), c_prev_);
+}
+
+TEST(CNSolverTest, MethodCalcA)
+{
+    double c_init = 0.45 * 51410;
+    char electrode_type = 'p';
+    double num_spatial_pts = 100;
+
+    double dt = 0.1;
+    double R = 8.5e-6;
+    double D = 1e-14;
+
+    CNSolver solver_instance = CNSolver(c_init, electrode_type, num_spatial_pts);
+
+    EXPECT_NEAR(solver_instance.calc_A(dt, R, D), 0.138408304, 1e-6);
+}
+
+TEST(CNSolverTest, MethodCalcB)
+{
+    double c_init = 0.45 * 51410;
+    char electrode_type = 'p';
+    double num_spatial_pts = 100;
+
+    double dt = 0.1;
+    double R = 8.5e-6;
+    double D = 1e-14;
+
+    CNSolver solver_instance = CNSolver(c_init, electrode_type, num_spatial_pts);
+
+    EXPECT_NEAR(solver_instance.calc_B(dt, R, D), 5.88235e-9, 1e-14);
 }
